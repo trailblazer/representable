df5f9ea3abe3186b483948855c3da0d77c2afd26
dmuneras <dms@inpay.com>
2021-09-09 02:36:48 +0200

    refactor: Update tests according to suggestions

--- test/object_test.rb
+++ test/object_test.rb
@@ -22,23 +22,28 @@ class ObjectTest < MiniTest::Spec
   let(:source) { Song.new("The King Is Dead", Album.new("Ruiner", [Song.new("In Vino Veritas II")])) }
   let(:target) { Song.new }
 
-  it do
-    representer.prepare(target).from_object(source)
+  describe ".from_object" do
+    describe "when using object with nested records as source" do
+      it "copies nested information to target object" do
+        representer.prepare(target).from_object(source)
 
-    _(target.title).must_equal "The King Is Dead"
-    _(target.album.name).must_equal "RUINER"
-    _(target.album.songs[0].title).must_equal "IN VINO VERITAS II"
+        _(target.title).must_equal("The King Is Dead")
+        _(target.album.name).must_equal("RUINER")
+        _(target.album.songs[0].title).must_equal("IN VINO VERITAS II")
+      end
+    end
+
+    describe "when nested object is nil" do
+      let(:target) do
+        representer.prepare(Song.new("The King Is Dead")).from_object(Song.new)
+      end
+
+      it "overrides value that was not nil" do
+        _(target.title).must_be_nil
+      end
+    end
   end
 
-  # ignore nested object when nil
-  it do
-    representer.prepare(Song.new("The King Is Dead")).from_object(Song.new)
-
-    _(target.title).must_be_nil # scalar property gets overridden when nil.
-    _(target.album).must_be_nil # nested property stays nil.
-  end
-
-  # to_object
   describe "#to_object" do
     representer!(module: Representable::Object) do
       property :title
@@ -52,25 +57,27 @@ class ObjectTest < MiniTest::Spec
       end
     end
 
-    it do
+    it "transforms the object using representer" do
       representer.prepare(source).to_object
-      _(source.album.name).must_equal "Live"
-      _(source.album.songs[0].title).must_equal 1
+
+      _(source.album.name).must_equal("Live")
+      _(source.album.songs[0].title).must_equal(1)
     end
-  end
 
-  describe ".format_engine" do
-    let(:decorator) do
-      class DecoratedObject < Representable::Decorator
-        include Representable::Object
-
-        property :city, as: :city_name
+    describe "when used as decorator" do
+      representer!(name: :decorator ,module: Representable::Object, decorator: true) do
+        property :name, getter: ->(represented:, **) {
+          represented.name = "#{represented.name} is the best team of America"
+        }
       end
 
-      DecoratedObject
-    end
+      let(:target) { Album.new("Atletico Nacional") }
 
-    it { decorator.must_equal(ObjectTest::DecoratedObject) }
-    it { decorator.format_engine.must_equal(Representable::Object) }
+      it "transforms object" do
+        decorator.prepare(target).to_object
+
+        _(target.name).must_equal("Atletico Nacional is the best team of America")
+      end
+    end
   end
 end
